977ca5c1b6a4e51323e4744732906283b681ac8a
wyxxxcat <1520358997@qq.com>
2024-04-08 22:30:39 +0800

    4.8

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(nums: i32) -> i32 {
+    let ans: i32 = if nums > 40 { nums } else { nums * 2 };
+    return ans;
+}
 
 // Don't modify this function!
 #[test]
