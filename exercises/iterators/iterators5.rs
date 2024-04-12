101fe18fb9910dce167182362af975670617dfdf
wyxxxcat <1520358997@qq.com>
2024-04-12 23:37:15 +0800

    update

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -11,7 +11,6 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -33,9 +32,13 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 }
 
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
-    // map is a hashmap with String keys and Progress values.
-    // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    let mut ans = 0;
+    for (s,p) in map.iter() {
+        if *p == value {
+            ans += 1;
+        }
+    }
+    ans
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -51,10 +54,15 @@ fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progres
 }
 
 fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
-    // collection is a slice of hashmaps.
-    // collection = [{ "variables1": Complete, "from_str": None, ... },
-    //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    let mut ans = 0;
+    for map in collection {
+        for (s,p) in map {
+            if *p == value {
+                ans += 1;
+            }
+        }
+    }
+    ans
 }
 
 #[cfg(test)]
--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -18,11 +18,10 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,13 +32,15 @@ fn main() {
         create_non_empty_list()
     );
 }
-
+use crate::List::{Cons, Nil};
 pub fn create_empty_list() -> List {
-    todo!()
+    let list = Box::new(Nil);
+    *list
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let list = Cons(1, Box::new(Nil));
+    list
 }
 
 #[cfg(test)]
